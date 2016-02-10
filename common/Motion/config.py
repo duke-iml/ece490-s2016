3525094d853807b15655cac5a14fcf3d2cd12d41
krishauser <hauser.kris@gmail.com>
2016-02-10 01:57:07 -0500

    Fixed problems with dual-arm motion queue

--- common/Motion/config.py
+++ common/Motion/config.py
@@ -1,14 +1,14 @@
 #################### SYSTEM CONFIGURATION VARIABLES #######################
 
-#change this to None to use the default mode linked to by libebolabot.so
+#change this to "client" to use the MotionServer_X program
 #change this to "kinematic" to test in kinematic mode
 #change this to "physical" if you are ready to test on the real robot
 #mode = 'client'
-#mode = 'kinematic'
-mode = 'physical'
+mode = 'kinematic'
+#mode = 'physical'
 
-#relative path, assumes everything is run from the Ebolabot directory
-klampt_model = "klampt_models/ebolabot_col.rob"
+#relative path, assumes everything is run from the common directory
+klampt_model = "klampt_models/baxter_with_parallel_gripper_col.rob"
 #klampt_model = "klampt_models/hstar_amp1_col.rob"
 
 #Motion server address.  Use this if you are using a server on another
@@ -27,8 +27,8 @@ def setup(parse_sys=True):
     global mode,klampt_model,motion_server_addr
     import os
     import motion
-    ebolabot_root = os.getenv("EBOLABOT_PATH","./")
+    motion_root = os.getenv("MOTION_API_PATH","./")
     if parse_sys:
         parse_args()
     print "Loading Motion Module model",klampt_model
-    motion.setup(mode=mode,klampt_model=klampt_model,libpath=ebolabot_root,server_addr=motion_server_addr)
+    motion.setup(mode=mode,klampt_model=klampt_model,libpath=motion_root,server_addr=motion_server_addr)
