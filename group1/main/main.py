7dbeb73beef05dfd652a57dd6be651a2043cbf21
lynchdennis94 <lynch.dennis94@gmail.com>
2016-03-28 17:35:13 -0400

    A bare-bones skeleton file for the supervisor - needs more than just motion control code

---
+++ group1/main/main.py
@@ -0,0 +1,15 @@
+#The "supervisor" module for the Amazing Stowing Team
+
+import time
+import json
+
+if __name__ == "__main__":
+    #Initialize our robot instance
+        # Motion Control State Machine
+            # Bump functionality
+        # Image Capture Module
+        
+
+    # Load in the JSON file
+
+    # Call function to determine what object to pick up
