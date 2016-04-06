12a6836f481ec41c93b65798b63f947f2a278fb5
Cameron Givler <cameron.givler@duke.edu>
2016-04-06 18:04:12 -0400

    Bump.py is now an importable object

--- group1/motion_control/bump.py
+++ group1/motion_control/bump.py
@@ -22,73 +22,88 @@ from operator import add
 # motion.robot.right_planner = LimbPlanner(RIGHT)
 # motion.robot.arms_planner = LimbPlanner(BOTH)
 
-klampt_model = "common/klampt_models/baxter_col.rob"
-mode = "client"
+class Bumper:
+    def __init__(self):
+        klampt_model = "common/klampt_models/baxter_col.rob"
+        mode = "client"
 
-def run():
-    global kRobot
-    global pRobot
-    kRobot = world.robot(0)
-    lWrist = kRobot.link("left_wrist")
-    rWrist = kRobot.link("right_wrist")
-    pRobot = motion.robot
-    while True:
-        command = raw_input("Command: ")
-        l = pRobot.left_limb.sensedPosition()
-        r = pRobot.right_limb.sensedPosition()
-        q = pRobot.getKlamptSensedPosition()
-        kRobot.setConfig(q)
-        if command == 'q':
-            exit(0)
-        elif command == 'c':
-            print "right:", lWrist.getWorldPosition((0,0,0))
-            print "left:", rWrist.getWorldPosition((0,0,0))
-        elif command[0] == 'l':
-            config = make_tuple(command[1:])
-            iksolve(config, lWrist, pRobot.left_limb, pRobot.left_mq)
-        elif command[0] == 'r':
-            config = make_tuple(command[1:])
-            iksolve(config, rWrist, pRobot.right_limb, pRobot.right_mq)
-        elif command[0] == 'b':
-            if command[1] =='r':
-                config = map(add, make_tuple(command[2:]), rWrist.getWorldPosition((0,0,0)))
-                print "new:", config
-                iksolve(config, rWrist, pRobot.right_limb, pRobot.right_mq)
-            elif command[1] =='l':
-                config = map(add, make_tuple(command[2:]), lWrist.getWorldPosition((0,0,0)))
-                print "new:", config
-                iksolve(config, lWrist, pRobot.left_limb, pRobot.left_mq)
+        print "Loading Motion Module model", klampt_model
+        motion.setup(mode=mode,klampt_model=klampt_model,libpath="common/",)
+        res = motion.robot.startup()
+        if not res:
+            print "Error starting up Motion Module"
+            return
+        time.sleep(0.1)
+        world = WorldModel()
+        res = world.readFile(klampt_model)
+        if not res:
+            raise RuntimeError("Unable to load Klamp't model", klampt_model)
+        #self.robotPoser = RobotPoser(world.robot(0))
+        self.kRobot = world.robot(0)
+        self.pRobot = motion.robot
+
+    def run(self):
+        while True:
+            command = raw_input("Command: ")
+            l = pRobot.left_limb.sensedPosition()
+            r = pRobot.right_limb.sensedPosition()
+            q = pRobot.getKlamptSensedPosition()
+            kRobot.setConfig(q)
+            if command == 'q':
+                exit(0)
+            elif command == 'c':
+                print "right:", getRightArmCoords()
+                print "left:", getLeftArmCoords()
+            elif command[0] == 'l':
+                moveLeftArmTo(make_tuple(command[1:]))
+            elif command[0] == 'r':
+                moveRightArmTo(make_tuple(command[1:]))
+            elif command[0] == 'b':
+                coords = make_tuple(command[2:])
+                if command[1] =='r':
+                    bumpRight(coords)
+                elif command[1] =='l':
+                    bumpLeft(coords)
+                else:
+                    print "Unknown command"
             else:
                 print "Unknown command"
-        else:
-            print "Unknown command"
 
-def iksolve(config, kEE, pEE, mq):
-    goal = ik.objective(kEE,local=(0,0,0), world=config)
-    q = pRobot.getKlamptSensedPosition()
-    kRobot.setConfig(q)
-    if ik.solve(goal):
-        print "success!"
-        q = kRobot.getConfig()
-        mq.setRamp(pEE.configFromKlampt(q))
-    else:
-        print "failed. Residual:", ik.solver(goal).getResidual()
+    def getLeftArmCoords(self):
+        return self.kRobot.link("left_wrist").getWorldPosition((0,0,0))
+
+    def getRightArmCoords(self):
+        return self.kRobot.link("right_wrist").getWorldPosition((0,0,0))
+
+    def bumpLeft(self, coords):
+        config = map(add, coords, getLeftArmCoords())
+        moveLeftArmTo(config)
+
+    def bumpRight(self, coords):
+        config = map(add, coords, getRightArmCoords())
+        moveRightArmTo(config)
+
+    def moveRightArmTo(self, coords):
+        iksolve(coords, self.kRobot.link("right_wrist"), self.pRobot.right_limb, self.pRobot.right_mq)
+
+    def moveLeftArmTo(self, coords):
+        iksolve(coords, self.kRobot.link("left_wrist"), self.pRobot.left_limb, self.pRobot.left_mq)
+
+    def iksolve(self, config, kEE, pEE, mq):
+        goal = ik.objective(kEE,local=(0,0,0), world=config)
+        q = pRobot.getKlamptSensedPosition()
+        kRobot.setConfig(q)
+        if ik.solve(goal):
+            print "success!"
+            q = kRobot.getConfig()
+            mq.setRamp(pEE.configFromKlampt(q))
+        else:
+            print "failed. Residual:", ik.solver(goal).getResidual()
+
 
 if __name__ == "__main__":
     global robotPoser
-    print "widget_control.py: manually sends configurations to the Motion module"
+    print "bump.py: bumps the robot arm a specified distance"
     print
-
-    print "Loading Motion Module model",klampt_model
-    motion.setup(mode=mode,klampt_model=klampt_model,libpath="common/",)
-    res = motion.robot.startup()
-    if not res:
-        print "Error starting up Motion Module"
-        exit(1)
-    time.sleep(0.1)
-    world = WorldModel()
-    res = world.readFile(klampt_model)
-    if not res:
-        raise RuntimeError("Unable to load Klamp't model", klampt_model)
-    robotPoser = RobotPoser(world.robot(0))
-    run()
+    bump = Bumper()
+    bump.run()
