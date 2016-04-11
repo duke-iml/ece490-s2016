e3a17dd327fa299c2235a8f43b7c372fcd6f30d6
Chenyu Li <chenyu.li@duke.edu>
2016-04-11 11:54:46 -0400

    first test of color histogram match using little bear

--- group3/perception/perception.py
+++ group3/perception/perception.py
@@ -69,15 +69,18 @@ def convertPc2ToNp(data):
         i = struct.unpack('>l',s)[0]
         # you can get back the float value by the inverse operations
         pack = ctypes.c_uint32(i).value
-        a = (pack & 0xFF000000)>> 24
-        r = (pack & 0x00FF0000)>> 16
-        g = (pack & 0x0000FF00)>> 8
-        b = (pack & 0x000000FF)
+        # a = (pack & 0xFF000000)>> 24
+        r1 = (pack & 0x00FF0000)>> 16
+        g1 = (pack & 0x0000FF00)>> 8
+        b1 = (pack & 0x000000FF)
+        r.append(r1)
+        g.append(g1)
+        b.append(b1)
         idx.append(i)
         i = i+1
     print "Point cloud count: " + str(len(xs))
-    print r,g,b,a
-    cloud = np.array([xs,ys,zs,idx])
+    # print r,g,b,a
+    cloud = np.array([xs,ys,zs,idx,r,g,b])
     cloud = cloud.transpose()
     nans = np.isnan(cloud)
     cloud[nans] = 0
@@ -118,15 +121,16 @@ def segmentation(cloud):
     object is cloud without index now
     """
     print 'start segmentation'
-    STEP = 50
-    o = 1
-    oo = 1
-    ooo = 1
-    object = copy.copy(cloud[:,0:3])
-    tree = cKDTree(object)
-    dist1,idx1 = tree.query(object,k=20, eps=0, p=2, distance_upper_bound=5)
-
-    dmin = len(object)/2
+    STEP = 15
+    o = 1 # how many points are searched
+    oo = 1 # number of iterations done for each plane
+    ooo = 1 # how many planes found
+    cloudcopy = copy.copy(cloud[:,0:3]) # delete the fourth column (index column)
+    tree = cKDTree(cloudcopy)
+    dist1,idx1 = tree.query(cloudcopy,k=20, eps=0, p=2, distance_upper_bound=5)
+    # print idx1
+    print len(cloudcopy)
+    dmin = len(cloudcopy)/2
     print "dmin"
     print dmin
     point_list = np.array([dmin])#index number of points in each iteration
@@ -134,10 +138,10 @@ def segmentation(cloud):
     edthall = np.median(dist1)
     print "edthall is "
     print edthall
-    leftpoint = np.ones(len(object))
+    leftpoint = np.ones(len(cloudcopy))
     listall = [point_list]
     show = 1
-    while max(leftpoint) > 0 and ooo <= 5:
+    while max(leftpoint) > 0 and ooo <= 10:
         print("segment ",ooo)
         r = np.zeros((1,4))
         while len(point_list) >0 and oo<=5000:
@@ -147,22 +151,23 @@ def segmentation(cloud):
             for k in range(0,number):
                 dmin = point_list[0]
                 edth = np.median(dist1[dmin-1])
-                n,v,p = np.linalg.svd(object[idx1[dmin-1,:],:])
+                n,v,p = np.linalg.svd(cloudcopy[idx1[dmin-1,:],:])
                 p = p.transpose()
-                dminmean = np.mean(object[idx1[dmin-1,:],:], axis=0)
+                dminmean = np.mean(cloudcopy[idx1[dmin-1,:],:], axis=0)
                 odth1 = np.zeros((20,1))
                 for i in range(0,20):
-                    odth1[i] = abs(np.dot((object[idx1[dmin-1,i],:] - dminmean),p[:,2])) 
+                    odth1[i] = abs(np.dot((cloudcopy[idx1[dmin-1,i],:] - dminmean),p[:,2])) 
                 odth = np.median(odth1)
                 pointss = np.zeros((20,4))
                 pointnum = np.zeros(20)
                 pointnum1 = np.zeros(20)
                 for i in range(0,20): 
-                    if dist1[dmin-1,i]<=abs(edthall) and dist1[dmin-1,i] <= abs(edth) and np.dot((object[idx1[dmin-1,i],:] - dminmean),p[:,2])<odth:
+                    if (dist1[dmin-1,i]<=min(0.003,abs(edthall)) and dist1[dmin-1,i] <= abs(edth)and np.dot((cloudcopy[idx1[dmin-1,i],:] - dminmean),p[:,2])<odth/3):
                         pointss [i,:] = cloud[idx1[dmin-1,i],:]
                         pointnum[i] = idx1[dmin-1,i]
                         leftpoint[idx1[dmin-1,i]] = 0
-                        object[idx1[dmin-1,i],0:3] = 0
+                        cloudcopy[idx1[dmin-1,i],0:3] = 0
+                pointss = pointss[pointss[:,1]!=0,:]
                 r = np.append(r,pointss,axis =0)
                 pointnum1 = pointnum
                 mask = (pointnum1 !=0)
@@ -195,7 +200,7 @@ def segmentation(cloud):
             else:
                 oo = oo+1
                 listall.append(point_list)# end of inside while loop
-        if len(r) > 1 :
+        if len(r) > 50:
             print np.size(r,axis= 0)
             print np.size(r,axis= 1)
             a = 'plane'+str(ooo)
@@ -206,19 +211,21 @@ def segmentation(cloud):
         pointss = np.zeros((20,4))
         pointnum = np.zeros(20)
         pointnum1 = np.zeros(20)
-        object = object[object[:,2]!=0]
-        tree = cKDTree(object)
-        dist1,idx1 = tree.query(object,k=20, eps=0, p=2, distance_upper_bound=5)
-        leftpoint = np.ones(len(object))
-        dmin = len(object)/2
+        cloudcopy = cloudcopy[cloudcopy[:,2]!=0]
+        tree = cKDTree(cloudcopy)
+        dist1,idx1 = tree.query(cloudcopy,k=20, eps=0, p=2, distance_upper_bound=5)
+        leftpoint = np.ones(len(cloudcopy))
+        dmin = len(cloudcopy)/2
         point_list = np.array([dmin])
         sellist = np.array([dmin])
         edthall = np.median(dist1)
+        print "edthall is "
+        print edthall
         listall = [point_list]# why this???
         ooo = ooo+1
         r = r[r[:,1]!=0 , :]
         print len(r)
-        print ("plane from segmentation: ", r)
+        print ("plane from segmentation: ")
     return r
 
 def com(cloud):
--- group3/perception/test_sub.py
+++ group3/perception/test_sub.py
@@ -5,21 +5,47 @@ from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 from scipy.spatial import KDTree, cKDTree
 import perception
+import color
 
 ROS_DEPTH_TOPIC = "/camera/depth/points"
 ROS_DEPTH_TOPIC = "/realsense/pc"
+# ROS_DEPTH_TOPIC = "/cloud_pcd"
 
 rospy.init_node("listener", anonymous=True)
 STEP = 25
 cloud = rospy.wait_for_message(ROS_DEPTH_TOPIC, PointCloud2)
-if perception.isCloudValidtest(cloud):
-    np_cloud = perception.convertPc2ToNptest(cloud)
+if perception.isCloudValid(cloud):
+    np_cloud = perception.convertPc2ToNp(cloud)
     # np_cloud, pointmean = perception.calPointCloud(np_cloud)
     np_cloud = perception.subtractShelf(np_cloud)
+    np_cloud =  np_cloud[np_cloud[:,0] != 0 , :]
+    np.savez('sub', np_cloud[:,0],np_cloud[:,1],np_cloud[:,2])
     cloud = np_cloud[np_cloud[:,0] != 0 , :]
+    # cloud = cloud[cloud[:,5] != 255,:]
     print len(cloud)
-    plan1 = perception.segmentationtest(cloud)
-    np.savez('subfinished', np_cloud[:,0],np_cloud[:,1],np_cloud[:,2])
+    print cloud 
+    # np.savez('object', cloud[:,0],cloud[:,1],cloud[:,2])
+    y,u,v = color.rgb_to_yuv(cloud[:,4],cloud[:,5],cloud[:,6])
+    print 'y', y
+    print 'u', u
+    print 'v', v
+    uv = [color.rgb_to_yuv(*rgb)[1:3] for rgb in cloud[:,4:7]]
+    # uv = [u,v]
+    print uv
+    hist = color.make_uv_hist(uv)
+    print hist
+    a = hist.sum()
+    print a
+    old_hist = np.load('hist.npz')
+    old_hist = old_hist['arr_0']
+    print old_hist.sum()
+    # np.savez('hist', hist)
+    # print old_hist
+    score = 2*np.minimum(hist,old_hist).sum()-np.maximum(hist,old_hist).sum()
+    print score
+    # print len(cloud)
+    # plane1 = perception.segmentation(cloud)
+    # np.savez('subfinished', plane1[:,0],plane1[:,1],plane1[:,2])
     # plane = perception.segmentation(np_cloud)
     print'finish sub '
     # print plane
