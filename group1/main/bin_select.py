03dadc488a9816635a8601294cbe2d88d89b8e91
vincecr <tekboi2010@gmail.com>
2016-04-20 18:23:00 -0400

    Added original # of items, and changed to percent free
    Added original # of items to account for tournament rules based only on
    original items . Made changes to the intiialization as well as
    calculations to change from percent full to percent free

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -4,26 +4,26 @@
 import time
 import json
 
-class binSelect:
+class binSelector:
     
     
     def __init__(self):
         #Create empty bin objects
         self.bin_dict={};
         #Keys are the number of the bin and the value is a list
-        #The list contains a list of items, number of items, and total volume 
-        self.bin_dict[1]=[[],0,0,0];
-        self.bin_dict[2]=[[],0,0,0];
-        self.bin_dict[3]=[[],0,0,0];
-        self.bin_dict[4]=[[],0,0,0];
-        self.bin_dict[5]=[[],0,0,0]; 
-        self.bin_dict[6]=[[],0,0,0];
-        self.bin_dict[7]=[[],0,0,0]; 
-        self.bin_dict[8]=[[],0,0,0];
-        self.bin_dict[9]=[[],0,0,0]; 
-        self.bin_dict[10]=[[],0,0,0];
-        self.bin_dict[11]=[[],0,0,0]; 
-        self.bin_dict[12]=[[],0,0,0];
+        #The list contains a list of items, current number of items,original number of items, total volume, and percent free
+        self.bin_dict[1]=[[],0,0,0,100];
+        self.bin_dict[2]=[[],0,0,0,100];
+        self.bin_dict[3]=[[],0,0,0,100];
+        self.bin_dict[4]=[[],0,0,0,100];
+        self.bin_dict[5]=[[],0,0,0,100]; 
+        self.bin_dict[6]=[[],0,0,0,100];
+        self.bin_dict[7]=[[],0,0,0,100]; 
+        self.bin_dict[8]=[[],0,0,0,100];
+        self.bin_dict[9]=[[],0,0,0,100]; 
+        self.bin_dict[10]=[[],0,0,0,100];
+        self.bin_dict[11]=[[],0,0,0,100]; 
+        self.bin_dict[12]=[[],0,0,0,100];
         self.maxbinvolume=20 #total volume constant for each bin 
         
     def addtoBin(self,itemid,itembin,itemvol):
@@ -31,15 +31,15 @@ class binSelect:
         binlist[0].append(itemid); #add itemid to contents list
         binlist[1]=binlist[1]+1; #add item to number of items 
         binlist[2]=binlist[2]+itemvol; #add volume to total volume
-        binlist[3]=binlist[2]*100/self.maxbinvolume; #recalculate percentage full
+        binlist[3]=100-binlist[2]*100/self.maxbinvolume; #recalculate percentage free
         self.bin_dict[itembin]=binlist;
         
-    def chooseBin(self):
+    def chooseBin(self,itemvol):
         #This method selects a bin and returns that to the caller.
         placeholder=1
         
     def printBin(self):
         for key in self.bin_dict.keys():
             binlist=self.bin_dict[key];
-            print "bin ",key," contains ",binlist[0]," with ",binlist[1]," items taking up ",binlist[2]," units or ",binlist[3],"%";
+            print "bin ",key," contains ",binlist[0]," with ",binlist[1]," items taking up ",binlist[2]," units or ",binlist[3],"% free";
             
