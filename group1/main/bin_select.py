2477d67271ef4b226ca4b6a129fd67419cb27540
vincecr <tekboi2010@gmail.com>
2016-04-20 16:21:19 -0400

    Added percentage full field to dict
    Percentage full field added to each initialized bin. Also reformatted
    lines to save space

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -12,18 +12,12 @@ class binSelect:
     maxbinvolume=20 #total volume constant for each bin 
     def __init__(self):
         #Create empty bin objects
-        bin_dict[1]=[[],0,0];
-        bin_dict[2]=[[],0,0];
-        bin_dict[3]=[[],0,0];
-        bin_dict[4]=[[],0,0];
-        bin_dict[5]=[[],0,0];
-        bin_dict[6]=[[],0,0];
-        bin_dict[7]=[[],0,0];
-        bin_dict[8]=[[],0,0];
-        bin_dict[9]=[[],0,0];
-        bin_dict[10]=[[],0,0];
-        bin_dict[11]=[[],0,0];
-        bin_dict[12]=[[],0,0];
+        bin_dict[1]=[[],0,0,0]; bin_dict[2]=[[],0,0,0];
+        bin_dict[3]=[[],0,0,0]; bin_dict[4]=[[],0,0,0];
+        bin_dict[5]=[[],0,0,0]; bin_dict[6]=[[],0,0,0];
+        bin_dict[7]=[[],0,0,0]; bin_dict[8]=[[],0,0,0];
+        bin_dict[9]=[[],0,0,0]; bin_dict[10]=[[],0,0,0];
+        bin_dict[11]=[[],0,0,0]; bin_dict[12]=[[],0,0,0];
         
     def addtoBin(self,itemid,itembin,itemvol):
         binlist=bin_dict[itembin]; #convert to temp local var 
