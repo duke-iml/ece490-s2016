2eb43d5955f5303fe769dedbec7499d63a1e09ad
vincecr <tekboi2010@gmail.com>
2016-04-20 15:54:29 -0400

    Initialize 12 bins in the dictionary
    Creates 12 entries into the bin dictionary each containing a list which
    contains a list of items, number of items, and the volume consumed

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -7,11 +7,25 @@ import json
 
 class binSelect:
     bin_dict={}
+    #Keys are the number of the bin and the value is a list
+    #The list contains a list of items, number of items, and total volume 
 
-    def __init__(self,init_bin_dict):
-        bin_dict=init_bin_dict
-
-
+    def __init__(self):
+        bin_dict[bin1]=[[],0,0];
+        bin_dict[bin2]=[[],0,0];
+        bin_dict[bin3]=[[],0,0];
+        bin_dict[bin4]=[[],0,0];
+        bin_dict[bin5]=[[],0,0];
+        bin_dict[bin6]=[[],0,0];
+        bin_dict[bin7]=[[],0,0];
+        bin_dict[bin8]=[[],0,0];
+        bin_dict[bin9]=[[],0,0];
+        bin_dict[bin10]=[[],0,0];
+        bin_dict[bin11]=[[],0,0];
+        bin_dict[bin12]=[[],0,0];
+        
+    def addtoBin(self,targetitem,targetbin):
+        
     def chooseBin(self):
         #This method selects a bin and returns that to the caller.
         placeholder=1
