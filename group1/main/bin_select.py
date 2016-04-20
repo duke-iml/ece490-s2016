d0d247d23e2b75436abfbff1903c55fb93ea91e4
vincecr <tekboi2010@gmail.com>
2016-04-20 16:57:06 -0400

    Sundry Fixes
    A series of fixes added to move the code from a mere decoration to
    something that the computer can understand

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -6,33 +6,41 @@ import time
 import json
 
 class binSelect:
-    bin_dict={}
-    #Keys are the number of the bin and the value is a list
-    #The list contains a list of items, number of items, and total volume 
-    maxbinvolume=20 #total volume constant for each bin 
+    
+    
     def __init__(self):
         #Create empty bin objects
-		bin_dict={};
-        bin_dict[1]=[[],0,0,0]; bin_dict[2]=[[],0,0,0];
-        bin_dict[3]=[[],0,0,0]; bin_dict[4]=[[],0,0,0];
-        bin_dict[5]=[[],0,0,0]; bin_dict[6]=[[],0,0,0];
-        bin_dict[7]=[[],0,0,0]; bin_dict[8]=[[],0,0,0];
-        bin_dict[9]=[[],0,0,0]; bin_dict[10]=[[],0,0,0];
-        bin_dict[11]=[[],0,0,0]; bin_dict[12]=[[],0,0,0];
+        self.bin_dict={};
+        #Keys are the number of the bin and the value is a list
+        #The list contains a list of items, number of items, and total volume 
+        self.bin_dict[1]=[[],0,0,0];
+        self.bin_dict[2]=[[],0,0,0];
+        self.bin_dict[3]=[[],0,0,0];
+        self.bin_dict[4]=[[],0,0,0];
+        self.bin_dict[5]=[[],0,0,0]; 
+        self.bin_dict[6]=[[],0,0,0];
+        self.bin_dict[7]=[[],0,0,0]; 
+        self.bin_dict[8]=[[],0,0,0];
+        self.bin_dict[9]=[[],0,0,0]; 
+        self.bin_dict[10]=[[],0,0,0];
+        self.bin_dict[11]=[[],0,0,0]; 
+        self.bin_dict[12]=[[],0,0,0];
+        self.maxbinvolume=20 #total volume constant for each bin 
         
     def addtoBin(self,itemid,itembin,itemvol):
-        binlist=bin_dict[itembin]; #convert to temp local var 
+        binlist=self.bin_dict[itembin]; #convert to temp local var 
         binlist[0].append(itemid); #add itemid to contents list
         binlist[1]=binlist[1]+1; #add item to number of items 
         binlist[2]=binlist[2]+itemvol; #add volume to total volume
-        binlist[3]=binlist[2]/maxbinvolume; #recalculate percentage full
+        binlist[3]=binlist[2]*100/self.maxbinvolume; #recalculate percentage full
+        self.bin_dict[itembin]=binlist;
         
     def chooseBin(self):
         #This method selects a bin and returns that to the caller.
         placeholder=1
         
     def printBin(self):
-        for key in bin_dict.keys():
-            binlist=bin_dict[key];
+        for key in self.bin_dict.keys():
+            binlist=self.bin_dict[key];
             print "bin ",key," contains ",binlist[0]," with ",binlist[1]," items taking up ",binlist[2]," units or ",binlist[3],"%";
             
