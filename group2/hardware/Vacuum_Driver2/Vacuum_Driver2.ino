34204ce01ae49559ae3e87a577ea12f4e3ddf8a8
group2 <group2@pse-jm198-labpc.egr.duke.edu>
2016-05-02 17:19:31 -0400

    set pinMode, print ID on setup()

--- group2/hardware/Vacuum_Driver2/Vacuum_Driver2.ino
+++ group2/hardware/Vacuum_Driver2/Vacuum_Driver2.ino
@@ -8,22 +8,26 @@ void setup() {
   while (!Serial) {
     ; // wait for port to connect
   }
+  
+  pinMode(vacuum, OUTPUT);
   //Serial.println("Vacuum Driver Arduino");
+  delay(1000); 
+  Serial.println("c");
+
 }
 
 
-void loop() {
-  Serial.println("c");
+void loop() { 
   while (Serial.available() > 0) {
     
     // 49 is ascii number for "1"
     // send "1" (49) --> vacuum state = 49-48 = 1 --> HIGH
     vacuum_state = int(Serial.read()) - 48;
     if (vacuum_state == 1) {
-      digitalWrite(53, HIGH);
+      digitalWrite(vacuum, HIGH);
     }
     else{
-      digitalWrite(53, LOW);
+      digitalWrite(vacuum, LOW);
     }
     Serial.println(vacuum_state);    
   }
