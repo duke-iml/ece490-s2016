b99d617f9412ee157b2d24eb8217dcc38505251f
vincecr <tekboi2010@gmail.com>
2016-05-03 17:33:40 -0400

    Added item volume lookup
    Added item volume lookup to class so that volumes can be retreived with
    in the class only by item ID

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -26,7 +26,8 @@ class binSelector:
         self.bin_dict['12']=[[],0,0,0,100,0];
         self.maxbinvolume=1485 #total volume constant for each bin 
         
-    def addtoBin(self,itemid,itembin,itemvol):
+    def addtoBin(self,itemid,itembin):
+        itemvol=self.item_lookup[itemid];
         binlist=self.bin_dict[str(itembin)]; #convert to temp local var 
         binlist[0].append(itemid); #add itemid to contents list
         binlist[1]=binlist[1]+1; #add item to number of items 
@@ -88,23 +89,65 @@ class binSelector:
             print "bin ",key," contains ",binlist[0]," w/ ",binlist[1]," items ",binlist[4],"% free for ",binlist[2],"points with ",binlist[5]," strikes";
             
     def test(self):
-        self.addtoBin(1,1,100)
-        self.addtoBin(2,1,75)
-        self.addtoBin(3,1,120)
-        self.addtoBin(4,1,50)
-        self.addtoBin(5,1,90)
-        self.addtoBin(6,1,100)
-        self.addtoBin(7,2,100)
-        self.addtoBin(8,2,75)
-        self.addtoBin(9,4,100)
-        self.addtoBin(10,5,200)
-        self.addtoBin(11,5,50)
-        self.addtoBin(12,5,50)
-        self.addtoBin(13,5,50)
-        self.addtoBin(14,5,50)
-        self.addtoBin(15,7,100)
-        self.addtoBin(16,7,100)
-        self.addtoBin(17,7,100)
-        self.addtoBin(18,7,100)
-        self.addtoBin(19,7,100)
-        self.addtoBin(20,11,175)
+        self.addtoBin("folgers_classic_roast_coffee",1)
+        self.addtoBin("elmers_glue_all",1)
+        self.addtoBin("hanes_tube_socks",1)
+        self.addtoBin("womens_knit_gloves",1)
+        self.addtoBin(5,1)
+        self.addtoBin(6,1)
+        self.addtoBin(7,2)
+        self.addtoBin(8,2)
+        self.addtoBin(9,4)
+        self.addtoBin(10,5)
+        self.addtoBin(11,5)
+        self.addtoBin(12,5)
+        self.addtoBin(13,5)
+        self.addtoBin(14,5)
+        self.addtoBin(15,7)
+        self.addtoBin(16,7)
+        self.addtoBin(17,7)
+        self.addtoBin(18,7)
+        self.addtoBin(19,7)
+        self.addtoBin(20,11)
+
+    def itemlookup(self):
+        self.item_lookup={};
+        self.item_lookup["i_am_a_bunny_hardcover_book"]=200;
+        self.item_lookup["laugh_out_loud_jokes_for_kids_paperback_book"]=200;
+        self.item_lookup["scotch_bubble_mailer"]=200;
+        self.item_lookup["up_and_up_glucose_tablets"]=200;
+        self.item_lookup["dasani_water_bottle"]=200;
+        self.item_lookup["rawlings_recreational_use_baseball"]=200;
+        self.item_lookup["folgers_classic_roast_coffee"]=200;
+        self.item_lookup["elmers_glue_all"]=200;
+        self.item_lookup["hanes_tube_socks"]=200;
+        self.item_lookup["womens_knit_gloves"]=200;
+        self.item_lookup["cherokee_easy_tee_t_shirt"]=200;
+        self.item_lookup["peva_shower_curtain_liner"]=200;
+        self.item_lookup["cloud_b_plush_bear"]=200;
+        self.item_lookup["boots_and_barkley_beefhide_bones"]=200;
+        self.item_lookup["plush_puppies_squeakin_eggs"]=200;
+        self.item_lookup["cool_shot_mini_glue_sticks"]=200;
+        self.item_lookup["creativity_street_chenille_stems_100_count"]=200;
+        self.item_lookup["40w_soft_white_lightbulb"]=200;
+        self.item_lookup["safety_1st_ultra_clear_outlet_plugs"]=200;
+        self.item_lookup["oral_b_cavity_defense_toothbrush"]=200;
+        self.item_lookup["dr_browns_natural_flow_bottle_brush"]=200;
+        self.item_lookup["command_hooks"]=200;
+        self.item_lookup["easter_turtle_figural_sippy_cup"]=200;
+        self.item_lookup["fiskars_scissors_5inch_blunt_tip_red"]=200;
+        self.item_lookup["scotch_cloth_duct_tape"]=200;
+        self.item_lookup["woods_6ft_extension_cord"]=200;
+        self.item_lookup["platinum_pets_dog_bowl"]=200;
+        self.item_lookup["fitness_gear_3lb_neoprene_dumbbell"]=200;
+        self.item_lookup["rolodex_wire_mesh_jumbo_pencil_holder"]=200;
+        self.item_lookup["clorox_utility_brush"]=200;
+        self.item_lookup["kleenex_viva_paper_towels"]=200;
+        self.item_lookup["expo_white_board_care_eraser"]=200;
+        self.item_lookup["kleenex_cool_touch_tissue_box"]=200;
+        self.item_lookup["ticonderoga_#2_pencils_12_count"]=200;
+        self.item_lookup["crayola_crayons"]=200;
+        self.item_lookup["jane_eyre_dvd"]=200;
+        self.item_lookup["dove_dry_oil_beauty_bar"]=200;
+        self.item_lookup["staples_3x5_ruled_index_cards"]=200;
+        
