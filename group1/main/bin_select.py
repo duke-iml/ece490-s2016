35ec8b6c32f6a0aa57d9beee452cdfb58ac0fc43
vincecr <tekboi2010@gmail.com>
2016-05-02 21:27:47 -0400

    Added start module to write the point values
    Point values added in

--- group1/main/bin_select.py
+++ group1/main/bin_select.py
@@ -11,28 +11,28 @@ class binSelector:
         #Create empty bin objects
         self.bin_dict={};
         #Keys are the number of the bin and the value is a list
-        #The list contains a list of items, current number of items,original number of items, total volume, and percent free
-        self.bin_dict[1]=[[],0,0,0,100];
-        self.bin_dict[2]=[[],0,0,0,100];
-        self.bin_dict[3]=[[],0,0,0,100];
-        self.bin_dict[4]=[[],0,0,0,100];
-        self.bin_dict[5]=[[],0,0,0,100]; 
-        self.bin_dict[6]=[[],0,0,0,100];
-        self.bin_dict[7]=[[],0,0,0,100]; 
-        self.bin_dict[8]=[[],0,0,0,100];
-        self.bin_dict[9]=[[],0,0,0,100]; 
-        self.bin_dict[10]=[[],0,0,0,100];
-        self.bin_dict[11]=[[],0,0,0,100]; 
-        self.bin_dict[12]=[[],0,0,0,100];
-        self.maxbinvolume=20 #total volume constant for each bin 
+        #The list contains a list of items, current number of items,points score, total volume, and percent free
+        self.bin_dict['1']=[[],0,0,0,100];
+        self.bin_dict['2']=[[],0,0,0,100];
+        self.bin_dict['3']=[[],0,0,0,100];
+        self.bin_dict['4']=[[],0,0,0,100];
+        self.bin_dict['5']=[[],0,0,0,100]; 
+        self.bin_dict['6']=[[],0,0,0,100];
+        self.bin_dict['7']=[[],0,0,0,100]; 
+        self.bin_dict['8']=[[],0,0,0,100];
+        self.bin_dict['9']=[[],0,0,0,100]; 
+        self.bin_dict['10']=[[],0,0,0,100];
+        self.bin_dict['11']=[[],0,0,0,100]; 
+        self.bin_dict['12']=[[],0,0,0,100];
+        self.maxbinvolume=1485 #total volume constant for each bin 
         
     def addtoBin(self,itemid,itembin,itemvol):
-        binlist=self.bin_dict[itembin]; #convert to temp local var 
+        binlist=self.bin_dict[str(itembin)]; #convert to temp local var 
         binlist[0].append(itemid); #add itemid to contents list
         binlist[1]=binlist[1]+1; #add item to number of items 
         binlist[3]=binlist[3]+itemvol; #add volume to total volume
         binlist[4]=100-binlist[3]*100/self.maxbinvolume; #recalculate percentage free
-        self.bin_dict[itembin]=binlist;
+        self.bin_dict[str(itembin)]=binlist;
         
     def chooseBin(self,itemvol):
         #This method selects a bin and returns that to the caller.
@@ -43,20 +43,37 @@ class binSelector:
                 validlist.append(prediction);
         print(validlist);
 
-    def binSort(self, binlist):
-        #Sorts the binsin order of priority. 
+    
+    def comparator(self, a,b):
+        #Comparator for the bin sort module
+       print null
         
     def addPredict(self,itembin,itemvol):
         #Predicts the free space in a bin after a particular item is added to a specified bin. Returns total volume and percentage free 
-        estsize=self.bin_dict[itembin][1]+1;
-        orgsize=self.bin_dict[itembin][2];
-        oversize=estsize-1-orgsize;
+        estnumel=self.bin_dict[itembin][1]+1;#number of elements after addition
+        points=self.bin_dict[itembin][2];#points level (higher is better)
         estvol=self.bin_dict[itembin][3]+itemvol;
         estpf=100-estvol*100/self.maxbinvolume;
-        return itembin, estsize, oversize, estpf
-        
+        return itembin, estnumel, points, estpf
+
+    def start(self):
+        #copy the value of Calculate score of each bin and write it to position 2
+        for key in self.bin_dict.keys():
+            orgnum=self.bin_dict[key][1];
+            if orgnum>=5:
+                self.bin_dict[key][2]=20;
+                continue;
+            elif orgnum>=3 and orgnum<5:
+                self.bin_dict[key][2]=15;
+                continue;
+            elif orgnum>=1 and orgnum<3:
+                self.bin_dict[key][2]=10;
+                continue;
+            self.bin_dict[key][2]=0
+        print("Bin Selector has been intialized")
+            
     def printBin(self):
         for key in self.bin_dict.keys():
             binlist=self.bin_dict[key];
-            print "bin ",key," contains ",binlist[0]," with ",binlist[1]," items taking up ",binlist[3]," units or ",binlist[4],"% free";
+            print "bin ",key," contains ",binlist[0]," with ",binlist[1]," items taking up ",binlist[3]," units or ",binlist[4],"% free for ",binlist[2];
             
