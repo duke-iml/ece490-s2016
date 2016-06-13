abb1bdf3ddcfce459e1d179e32d9a6631ee6dc85
group3 <group3@pse-jm198-labpc.egr.duke.edu>
2016-06-13 14:42:05 -0400

    change

--- apc2016/Integration/motionControllerIntegration.py
+++ apc2016/Integration/motionControllerIntegration.py
@@ -65,8 +65,8 @@ from Group2Helper import Vacuum_Comms
 
 
 NO_SIMULATION_COLLISIONS = 1
-FAKE_SIMULATION = 1
-PHYSICAL_SIMULATION = 0
+FAKE_SIMULATION = 0
+PHYSICAL_SIMULATION = 1
 
 ALL_ARDUINOS = 0
 MOTOR = 0 or ALL_ARDUINOS
@@ -75,7 +75,7 @@ VACUUM = 0 or ALL_ARDUINOS
 SPEED = 3
 
 REAL_SCALE = False
-REAL_CAMERA = False
+REAL_CAMERA = True
 REAL_JSON = False
 REAL_PRESSURE = 1
 
@@ -4190,7 +4190,7 @@ class MyGLViewer(GLRealtimeProgram):
                 pass
             if all_stow_pick_poss is not None:
                 for stow_pick_pos in all_stow_pick_poss:
-                gldraw.xform_widget(([1,0,0,0,1,0,0,0,1], [stow_pick_pos[0], stow_pick_pos[1], -0.1]), 0.5, 0.1, fancy=False)
+                    gldraw.xform_widget(([1,0,0,0,1,0,0,0,1], [stow_pick_pos[0], stow_pick_pos[1], -0.1]), 0.5, 0.1, fancy=False)
                 
                 
         if SHOW_BIN_BOUNDS:
@@ -4695,11 +4695,11 @@ def run_controller(controller,command_queue):
                 visualizer.simworld.terrain(0).geometry().transform(*controller.perceptionTransform)
             elif c=='c':
                 print 'Viewing Tote: '
-                controller.viewToteAction(limb='right')
+                controller.viewToteAction(limb=DEFAULT_LIMB)
             elif c=='g':
                 visualizer.simworld.terrain(0).geometry().transform(*controller.perceptionTransformInv)
             elif c == '1':
-                controller.runPickFromTote(limb = 'right')
+                controller.runPickFromTote(limb=DEFAULT_LIMB)
 
             elif c == '2':
                 controller.moveToRestConfig()
