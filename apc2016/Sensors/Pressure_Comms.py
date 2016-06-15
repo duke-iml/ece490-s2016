d98705afd2e1bde3f196206754e07163453ff6f4
Hyunsoo Kim <hk94@duke.edu>
2016-06-15 15:56:30 -0400

    arduino code changes

--- apc2016/Sensors/Pressure_Comms.py
+++ apc2016/Sensors/Pressure_Comms.py
@@ -12,21 +12,79 @@ class CommPressure:
 
     # initialize the serial comm
     # def __init__(self, port="COM5"):
-    def __init__(self, port="/dev/ttyACM1"):
+    # def __init__(self, port="/dev/ttyACM1"):
+    #     # on linux use port="/dev/ttyUSB2"
+    #     # attempt to connect
+    #     while True:
+    #         try:
+    #             self.com = Serial(port, 9600)
+    #             print('connected to arduino\n')
+    #             break
+    #         except SerialException:
+    #             print 'Error: No device is on port' + port
+    #             print 'Type an alternative ttyUSB number or a full port name'
+    #             port = input('Port = ')
+    #             if port.isdigit():
+    #                 port = '/dev/ttyUSB' + port
+    #     time.sleep(0.5)
+
+    # initialize the serial comm
+    def __init__(self, port="COM7"):
         # on linux use port="/dev/ttyUSB2"
         # attempt to connect
-        while True:
-            try:
-                self.com = Serial(port, 9600)
-                print('connected to arduino\n')
-                break
-            except SerialException:
-                print 'Error: No device is on port' + port
-                print 'Type an alternative ttyUSB number or a full port name'
-                port = input('Port = ')
-                if port.isdigit():
-                    port = '/dev/ttyUSB' + port
-        time.sleep(0.5)
+        foundPort = False
+        ports = list(serial.tools.list_ports.comports())
+        portArray = []
+        for p in ports:
+            print p
+            if "Arduino Micro" in p[1] or "Arduino Micro" in p[2] or "VID:PID=2a03:0042" in p[1] or "VID:PID=2a03:0042" in p[2]:
+
+                # print "Port =", p[0]
+                port = p[0]
+                # print port
+                portArray.append(port)
+                foundPort = True
+        if foundPort == False:
+            print "No Port Found (Arduino Micro)"
+
+        for i in range(len(portArray)):
+            port = portArray[i]
+            while True:
+                try:
+                    self.com = Serial(port, 9600)
+                    break
+                except SerialException:
+                    print 'Error: No device is on port' + port
+                    print 'Type an alternative ttyUSB number or a full port name'
+                    port = input('Port = ')
+                    if port.isdigit():
+                        port = '/dev/ttyUSB' + port
+
+            self.com.setDTR(False)
+            time.sleep(1)
+            # toss any data already received, see
+            # http://pyserial.sourceforge.net/pyserial_api.html#serial.Serial.flushInput
+            self.com.flushInput()
+            self.com.setDTR(True)
+
+            print('connected to an arduino... checking ID...')
+            time.sleep(0.01)
+
+            arduinoID = self.com.readline()
+            while not ("c" in arduinoID) or ("l" in arduinoID) or ("r" in arduinoID):
+                print arduinoID
+                arduinoID = self.com.readline()
+                time.sleep(0.01)
+
+            # print "ID:", arduinoID
+            if "c" in arduinoID:
+                print "ID Checked (pressure sensor", arduinoID, ")"
+            else:
+                print "Wrong ID:", arduinoID
+
+
+
+
 
     def close(self):
         self.com.close()
