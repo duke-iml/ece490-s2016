0513ff53cb1f2d150bce51ae51e1b4a50f61ef54
motion <motion@scovel.egr.duke.edu>
2016-07-19 14:18:03 -0400

    Removed old SSPP version

--- common/SSPP/services/topic_server.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from topic import TopicServer
-
-print "Starting topic server on localhost:4567"
-service = TopicServer(('localhost',4567))
-#processes messages at 100Hz
-service.run(100)
